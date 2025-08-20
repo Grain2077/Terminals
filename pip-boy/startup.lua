3a8d63a6bab59df33a48bdb50181e44e60f27fd5
Grain2077 <108996780+Grain2077@users.noreply.github.com>
2025-08-20 01:50:12 +0100

    Update startup.lua

--- pip-boy/startup.lua
+++ pip-boy/startup.lua
@@ -21,7 +21,7 @@ if fs.exists("user.txt") then
  username = f.readAll()
  f.close()
 else
-  textutils.slowPrint("^^^^PIP-OS(R) V3.1.0.0^^^^")
+  textutils.slowPrint("^^^^PIP-OS(R) V3.2.0.0^^^^")
   textutils.slowWrite("Enter User: ")
  username = read()  -- user types their username
  
@@ -32,7 +32,7 @@ else
 end
 
 --Print Information
-textutils.slowPrint("^^^^PIP-OS(R) V3.1.0.0^^^^")
+textutils.slowPrint("^^^^PIP-OS(R) V3.2.0.0^^^^")
 print("")
 print("")
 os.sleep(0.2)
